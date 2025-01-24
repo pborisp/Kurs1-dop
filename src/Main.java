22889d3cc15626725964258b8e80059b29bbf9a5
Slava ASUS <popovbs@gmail.com>
2025-01-25 00:53:09 +0300

    Tasks 1,2,3

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,26 @@ public class Main {
         int c=a/b, d=a%b;
         System.out.println("Выходит "+c+" и "+d+" в остатке");
 
+        // Задание 2
+        System.out.println();
+        System.out.println("Задание 2:");
+        byte x=99;
+        int A;
+        int X=x/10, Y=x%10;
+        A=X+Y;
+        System.out.println("Сумма чисел двузначного числа "+x+" равна "+A);
+
+        // Задание 3
+        System.out.println();
+        System.out.println("Задание 3:");
+        int three=888;
+        int Res;
+        int first, second, third;
+        first=three/100;
+        second=three%100/10;
+        third=three%100%10;
+        System.out.println("Сумма чисел трехзначного числа "+three+
+                " равна "+(first+second+third));
+
     }
 }
