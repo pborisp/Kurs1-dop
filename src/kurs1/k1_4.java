cd348314ae63a98bcffe7b9cb2a33a77b20a0f85
Slava ASUS <popovbs@gmail.com>
2025-01-25 20:15:16 +0300

    Tasks 1: add comment with count task

--- src/kurs1/k1_4.java
+++ src/kurs1/k1_4.java
@@ -2,6 +2,8 @@ package kurs1;
 
 public class k1_4 {
     public static void main(String[] args) {
+        // Задача 1
+        System.out.println("Задача №1:");
         int value = 1005 , number = 4 , count = 5 ;
         while (count > 0) {
             if (value % number == 0) {
