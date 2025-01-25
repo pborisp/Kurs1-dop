decd598401b0d24a2378ebb9e62187f69753ce93
Slava ASUS <popovbs@gmail.com>
2025-01-25 20:33:10 +0300

    Tasks 1,2

--- src/kurs1/k1_4.java
+++ src/kurs1/k1_4.java
@@ -4,13 +4,24 @@ public class k1_4 {
     public static void main(String[] args) {
         // Задача 1
         System.out.println("Задача №1:");
-        int value = 1005 , number = 4 , count = 5 ;
+        int value = 1005, number = 4, count = 5;
         while (count > 0) {
             if (value % number == 0) {
-                System.out.println(value) ;
-                count -- ;
+                System.out.println(value);
+                count--;
             }
-            value ++ ;
+            value++;
         }
+
+        // Задача 2
+        System.out.println();
+        System.out.println("Задача №2:");
+        int count2 = 10, firstNumber = 1;
+        int n = 2;
+        for (int N = 0; N < count2; N++) {
+            System.out.print(firstNumber + "  ");
+            firstNumber += n;
+        }
+
     }
 }
