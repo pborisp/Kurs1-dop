57007202c561d05fb1bde018ffde096565f7b77a
Slava ASUS <popovbs@gmail.com>
2025-01-25 16:24:46 +0300

    Tasks 1

--- src/kurs1/k1_2.java
+++ src/kurs1/k1_2.java
@@ -2,6 +2,12 @@ package kurs1;
 
 public class k1_2 {
     public static void main(String[] args) {
-
+        int p;
+        p = 8;
+        if (p % 2 == 0 && p != 0) {
+            System.out.println("Число четное");
+        } else {
+            System.out.println("Число нечетное");
+        }
     }
 }
