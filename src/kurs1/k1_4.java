0ec7fc60e60c4a0b1072dd98450effffa1425292
Slava ASUS <popovbs@gmail.com>
2025-01-25 20:12:42 +0300

    Tasks 1

--- src/kurs1/k1_4.java
+++ src/kurs1/k1_4.java
@@ -1,4 +1,14 @@
 package kurs1;
 
 public class k1_4 {
+    public static void main(String[] args) {
+        int value = 1005 , number = 4 , count = 5 ;
+        while (count > 0) {
+            if (value % number == 0) {
+                System.out.println(value) ;
+                count -- ;
+            }
+            value ++ ;
+        }
+    }
 }
