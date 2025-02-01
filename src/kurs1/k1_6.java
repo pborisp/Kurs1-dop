bdb38f17f885df4f29ea11f95301cd90fdba9519
Slava ASUS <popovbs@gmail.com>
2025-02-01 17:16:09 +0300

    Tasks 1

---
+++ src/kurs1/k1_6.java
@@ -0,0 +1,10 @@
+package kurs1;
+
+public class k1_6 {
+    public static void main(String[] args) {
+        // 1.6 Массивы
+        // Задание 1
+        System.out.println("Задание 1:");
+
+    }
+}
