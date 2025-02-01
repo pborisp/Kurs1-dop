b18a958df1a0ea6a89e8d8120ff6f5f5d1387880
Slava ASUS <popovbs@gmail.com>
2025-02-01 19:49:39 +0300

    Tasks 1,2

--- src/kurs1/k1_6.java
+++ src/kurs1/k1_6.java
@@ -26,5 +26,43 @@ public class k1_6 {
         System.out.println();
         System.out.println("Элекмент масиива с индексом " + iMax + " является самым большим в массиве. Его размер: " + max);
         System.out.println();
+
+        // Задание 2
+        System.out.println("Задание 2:");
+        size = 11;
+        double[] numsDouble = new double[size];
+        for (i = 0; i < size; i++) {
+            double x = Math.random();
+            numsDouble[i] = Math.ceil(x*100)/100;
+            System.out.print(" " + numsDouble[i] + " ");
+        }
+        System.out.println();
+        int countX = 1; // максимальное значение счетчика
+        double number; // значение одинаковых чисел
+        int numberCount = 0; // счетчик одинаковых чисел
+        double numberX = 0; // значение числа, повторяющегося чаще всех
+        double numberY = 0; // значение числа, с одинаковым кол-вом повторений
+        int y, z; // индексы
+        for (y = 0; y < size; y++) {
+            numberCount = 0;
+            for (z = 0; z < size; z++) {
+                if (numsDouble[z] == numsDouble[y]) {
+                    numberCount++;
+                }
+            }
+            if (numberCount > countX) {
+                countX = numberCount;
+                numberX = numsDouble[y];
+            } else if (numberCount == countX && numberX != numsDouble[y] && numberCount > 1) {
+                numberY = numsDouble[y];
+            }
+        }
+        if (numberY != 0 && numberY != numberX) {
+            System.out.println("-1");
+        } else if (numberX != 0) {
+            System.out.println("Элемент: " + numberX + " встречается чаще всего: " + countX + " раз");
+        } else {
+            System.out.println("Повторяющихся элементов нет");
+        }
     }
 }
