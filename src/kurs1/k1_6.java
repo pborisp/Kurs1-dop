3f86033f5903501818e63f95a651f6c3fa5b52b7
Slava ASUS <popovbs@gmail.com>
2025-02-01 17:30:51 +0300

    Tasks 1

--- src/kurs1/k1_6.java
+++ src/kurs1/k1_6.java
@@ -5,6 +5,26 @@ public class k1_6 {
         // 1.6 Массивы
         // Задание 1
         System.out.println("Задание 1:");
-
+        int size = 12;
+        int start = -16;
+        int end = 16;
+        int i, max, iMax;
+        int[] nums = new int[size];
+        for (i = 0; i < size; i++) {
+            double x = Math.random() * (end - start) - end;
+            nums[i] = (int) x;
+            System.out.print(" " + nums[i] + " ");
+        }
+        max = nums[0];
+        iMax = 0;
+        for (i = 1; i < size; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+                iMax = i;
+            }
+        }
+        System.out.println();
+        System.out.println("Элекмент масиива с индексом " + iMax + " является самым большим в массиве. Его размер: " + max);
+        System.out.println();
     }
 }
